a4654e6a6dc613d6c9431f2cbba891314693e584
rostyslav <rostik@telelan.com.ua>
2018-05-07 22:48:03 +0200

    login and register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'register',
@@ -10,6 +11,16 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hidePass = true;
+  hidePassRep = true;
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
   }
 
 }
