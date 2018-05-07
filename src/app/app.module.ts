ef5bcf36452a6a44a318aebb66ba04989aaf4a88
rostyslav <rostik@telelan.com.ua>
2018-05-07 21:26:52 +0200

    material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
+import { MaterialModule} from './material-module/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MainHeaderComponent } from './main-header/main-header.component';
     MainHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-module/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [MatToolbarModule]
+})
+export class MaterialModule { }
