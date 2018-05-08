42c2a3dee9926783522c99e448e622deab98abcb
rostyslav <rostik@telelan.com.ua>
2018-05-08 17:42:19 +0200

    added reister post

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'register',
@@ -11,7 +13,7 @@ export class RegisterComponent implements OnInit {
   hidePass = true;
   hidePassRep = true;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -24,6 +26,15 @@ export class RegisterComponent implements OnInit {
     return this.email.hasError('required') ? 'You must enter a value' :
       this.email.hasError('email') ? 'Not a valid email' :
         '';
+  }
+
+  register() {
+    const payload = {
+      email: this.email.value,
+      password: this.password.value
+    };
+    return this.http.post(`${environment.baseURL}/register`, payload).
+      subscribe(data => console.log('register', data));
   }
 
 }
