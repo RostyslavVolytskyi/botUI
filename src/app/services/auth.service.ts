b3c6701720f04af54a407ae22930c1f52b9c4ab6
rostyslav <rostik@telelan.com.ua>
2018-05-09 16:34:30 +0200

    minor fixes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { Observable, of } from 'rxjs';
-import { tap, delay } from 'rxjs/operators';
+import { tap, delay, share } from 'rxjs/operators';
 import {environment} from "../../environments/environment";
 import {ErrorMsgService} from "src/app/error-msg/error-msg.service";
 
@@ -19,21 +19,28 @@ export class AuthService {
 
   constructor(private http: HttpClient, private errorMsgService: ErrorMsgService) {}
 
-  login(): Observable<boolean> {
+  login(): Observable<any> {
     return of(true).pipe(
       delay(1000),
-      tap(val => this.isLoggedIn = true)
+      tap((data: any) => {
+        localStorage.setItem(STORAGE_TOKEN_KEY, JSON.stringify(data.token));
+        this.isLoggedIn = true;
+      }, (err) => {
+        this.errorMsgService.showErrorMsg(`${err.error.message}. ${err.error.error.errmsg}`);
+      }),
+      share()
     );
   }
 
   register(payload): Observable<any> {
     return this.http.post(`${environment.baseURL}/register`, payload).pipe(
-      tap(data => {
+      tap((data: any) => {
         localStorage.setItem(STORAGE_TOKEN_KEY, JSON.stringify(data.token));
         this.isLoggedIn = true;
       }, (err) => {
-        this.errorMsgService.showErrorMsg(err.message);
-      })
+        this.errorMsgService.showErrorMsg(`${err.error.message}. ${err.error.error.errmsg}`);
+      }),
+      share()
     )
   }
 
